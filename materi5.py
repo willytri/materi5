b3b85ff8ef06cc4f917c62cbc12eb8be6a65ce27
willytri <trideannoval@gmail.com>
2024-10-02 18:01:35 +0800

    Add files via upload

---
+++ materi5.py
@@ -0,0 +1,59 @@
+# nama = ["shandy",106,True,
+#         ["yuyun",145],3.96,
+#         [123,"alvito",False,3.66],
+#         "rehan"]
+
+# matkul = ["APD",
+#           "APL",
+#           "WEB",
+#           "jarkom",
+#           "BASDAT",
+#           "STRUKDURAT",
+#           "PTI",
+#           "KALKULUS",
+#           "PROBAS"]
+
+# print(matkul[6])
+
+# makanan = ["bakso","sate","soto","nasi goreng","mie ayam","ayam bakar","cumi goreng"]
+# print("sebelum: ")
+# print(makanan)
+# makanan.append("nasi goreng")
+# print("sesudah: ")
+
+# makanan.clear()
+# print(makanan)
+# del makanan{5}
+# data = makanan.pop(5)
+# print(makanan)
+# print(data)
+# print(makanan)
+# makanan.insert(2,"nasi goreng")
+# makanan[0] = ["ayam,bebek"]
+# print(makanan)
+
+# minuman = ["pocari", "josu", "es teler", "esteh jumbo", "bintang","minuman anak-anak" ]
+# print(minuman)
+
+# del minuman[2]
+# print(minuman)
+
+# minuman[4] = ["air putuh jumbo"]
+# print(minuman)
+
+# minuman.append("jus tomat")
+# print(minuman)
+
+# makanan = ["ayam","ikan",["bakso","soto","sate","ikan","bebek"],
+#            ["teh","kopi","air"]]
+# # print(makanan)
+# for i in makanan :
+#     # print(i),end=" "
+# for i in makanan :
+#     if isinstance(i, list):
+#         for j in i :
+#             print(j)
+            # print(j, end=" ")
+    # print(i)
+    # else:
+    #     print(i)
